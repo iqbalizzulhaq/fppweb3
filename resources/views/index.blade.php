242fcedbaeff764010308ab954c7eee0b5bf8cc2
Unknown <iqbal.izzulhaq3@gmail.com>
2018-05-09 17:02:16 +0700

    zayn jomblo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,7 +42,7 @@
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
 
-      <a class="navbar-brand page-scroll" href="#page-top"><i class="fa fa-play fa-rotate-270"></i> zayn JOMBLO/a> </div>
+      <a class="navbar-brand page-scroll" href="#page-top"><i class="fa fa-play fa-rotate-270"></i> zayn JOMBLo pol </a> </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
