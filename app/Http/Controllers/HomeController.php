62136e6f056f86ee007c99ca7ebdfa17483a3e04
Unknown <muzayyin937@gmail.com>
2018-05-11 09:11:20 +0700

    connect sign up & login + table user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
 }
