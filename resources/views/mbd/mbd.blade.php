c90ca63817e54b81e6e8710a2ca555835cf0a145
Unknown <iqbal.izzulhaq3@gmail.com>
2018-05-29 15:21:41 +0700

    hapus mbd

--- resources/views/mbd/mbd.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.head')
-
-@section('content')
-<a href="tabel_transaksi" class="btn btn-default btn-lg page-scroll">Tabel Transaksi</a> 
-<a href="tabel_pemesan" class="btn btn-default btn-lg page-scroll">Tabel Pemesan</a> 
-@endsection
